18f3d22b83b1566c9081f5dfe61eda4b0eaf6c0e
Jael Romero <f1ntch@outlook.com>
2020-06-07 09:49:37 +0200

    Update rocketchat.sh

--- rocketchat.sh
+++ rocketchat.sh
@@ -7,11 +7,18 @@
 # Version:		1.1
 # Requires: 	A valid gcloud project.
 
+#Configuratie
+INSTANCE_NAME="rocket-chat"
+ZONE="europe-west1-b"
+TYPE="n1-standard-1"
+IMAGE="ubuntu-os-cloud"
+FAMILY="ubuntu-1804-lts"
+TAGS="chat"
 
 if [[ $1 == "--help" ]]
 then
-	echo "to install the rocket-chat server run the script without arguments"
-	echo "to delete the rocket-chat server use -d or --delete"
+	echo "To install the rocket-chat server run the script without arguments"
+	echo "To delete the rocket-chat server use -d or --delete"
 	exit 0
 fi
 
@@ -21,25 +28,25 @@ then
 	# Check of er een instantie draait
 	INSTANCE=$( gcloud compute instances list | cut -d ' ' -f1 | grep rocket-chat ) &>/dev/null
 	if [[ $INSTANCE != 'rocket-chat' ]] &>/dev/null
-		then  echo "no existing rocket-chat instances found.."
+		then  echo "No existing rocket-chat instances found.."
 		exit 1 &>/dev/null
 	fi
 
 	INSTANCE_NAME="rocket-chat"
-	echo "deleting rocket-chat"
+	echo "Deleting rocket-chat"
 	sleep 4
-	echo "pleas wait.."
+	echo "Pleas wait.."
 	
 	# De quiet flag is steeds nodig, want anders vraagt gcloud om bevestiging.
 	# En als je dat dan naar dev/null stuurt, zie je dat niet en zit je vast!
 
 	# Verwijder insantie rocket-chat
-	echo "removing instance.. "
+	echo "Removing instance.. "
 	gcloud compute instances delete $INSTANCE_NAME --zone=europe-west1-b --quiet >&2 
-	echo "removing firewallrule"
+	echo "Removing firewallrule.."
 	# Verwijder firewallrule http3000
 	gcloud compute firewall-rules delete http3000 --quiet 2>/dev/null
-	echo "rocket-chat sucessfull deleted"
+	echo "Rocket-chat sucessfull deleted"
 else
 
 # Check of er reeds een instantie draait
@@ -47,28 +54,18 @@ INSTANCE=$( gcloud compute instances list | cut -d ' ' -f1 | grep rocket-chat )
 	if [[ $INSTANCE == 'rocket-chat' ]]
 		then  
 		URL=$( gcloud compute instances list | rev |cut -d ' ' -f3 | rev )
-		echo "rocket-chat already exists.."
-		echo "running on.."
+		echo "Rocket-chat already exists.."
+		echo "Its running on.."
 		echo "$URL/3000"
 		exit 2
 	fi
 
-echo "installing rocket-chat.."
+echo "Installing rocket-chat.."
 sleep 4
-echo "pleas wait.." 
-
-#Configuratie
-INSTANCE_NAME="rocket-chat"
-ZONE="europe-west1-b"
-TYPE="n1-standard-1"
-IMAGE="ubuntu-os-cloud"
-FAMILY="ubuntu-1804-lts"
-TAGS="chat"
-
-
+echo "Pleas wait.." 
 
 #Aanmaken instantie + startup-script
-echo "creating instance.."
+echo "Creating instance.."
 gcloud compute instances create $INSTANCE_NAME \
 --machine-type=$TYPE \
 --image-project=$IMAGE \
@@ -81,15 +78,13 @@ snap install rocketchat-server
 " 
 
 #Aanmaken firewall rule http3000
-echo "creating firewallrule.."
 gcloud compute firewall-rules create http3000 --allow=tcp:3000 --target-tags=$TAGS
 ipadress=$(gcloud compute instances list  | tr -s " " | cut -d " " -f5 | tail -n +2 )
 
-
 	if [[ $? -eq 0 ]]
 		then
-			echo "rocket-chat sucessfull installed"
-			echo "running on"
+			echo "Rocket-chat sucessfull installed"
+			echo "Its running on"
 			URL=$( gcloud compute instances list | rev |cut -d ' ' -f3 | rev )
 			echo "$URL/3000"
 			exit 0
